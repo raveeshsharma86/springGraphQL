be2c7aa16c0baa19f9b24fa43ec23470d0782946
Raveesh <newtonlovesheadbanging@gmail.com>
2025-02-27 20:26:10 +0530

    Basic Project setup

---
+++ src/test/java/com/accounts/springGraphQL/SpringGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounts.springGraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
